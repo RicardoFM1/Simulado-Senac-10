8dac0bb84ee4b186ec5fc865602d4313a0eca622
= <=>
2026-06-17 14:46:54 -0300

    feat(backend): Rota de mesas criadas.

--- Backend/Routes/index.php
+++ Backend/Routes/index.php
@@ -8,6 +8,8 @@ use Dotenv\Dotenv;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../Controllers/Usuario/usuarioController.php";
+require_once __DIR__ . "/../Controllers/Mesa/mesaController.php";
+
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
@@ -48,6 +50,26 @@ if ($rota === '/usuario/login') {
 
     if ($metodo === 'POST') {
         $controller->fazerLogin();
+    }
+}
+
+if ($rota === '/mesa') {
+    $controller = new MesaController();
+
+    if ($metodo === 'GET') {
+        $controller->listarMesas();
+    }
+
+    if ($metodo === 'POST') {
+        $controller->criarMesa();
+    }
+
+    if ($metodo === 'PUT') {
+        $controller->atualizarMesa();
+    }
+
+    if ($metodo === 'DELETE') {
+        $controller->deletarMesa();
     }
 }
 
